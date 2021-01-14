32e925654771171ddeaa52046bb5baa9bf429a73
Icaro Caires <icaro_de_caires@outlook.com>
2021-01-14 17:57:34 -0400

    Movimentos Possiveis Para as Peças

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 	
 	protected  Position position;
 	private Board board;
@@ -14,7 +14,26 @@ public class Piece {
 		return board;
 	}
 
-		
+	//Método abstrato, as subclasses deverão implementar	
+	public abstract boolean[][] possibleMoves();
 	
+	//Retorna possiveis movimentos da peça
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+	
+	//Retorna se existe pelo menos um movimento util para a peça
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat.length; j++) {
+				if (mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -52,6 +52,10 @@ public class ChessMatch {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("Nao existe peça na posicao de origem");
 		}
+		
+		if (!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("Nao existe Movimentos Possiveis para a peca escolhida");
+		}
 	}
 
 	// Adiciona a peça em determinada posição do tabuleiro
